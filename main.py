45042bba83e3c8af142d9a36a1a9fcde81d9aa8b
ericgr3gory <eric@e-gregory.com>
2025-01-31 20:48:20 -0800

    minr

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 import requests
 
 load_dotenv()
-#LINK = os.getenv('BEST_BUY_LINK')
+LINK = os.getenv('BEST_BUY_LINK')
 #LINK_5000 = os.getenv('BEST_BUY_LINK_5000')
 #LINK_TEST = os.getenv('BEST_BUY_LINK_TEST')
-LINK = os.getenv('BEST_BUY_giga_5090')
+#LINK = os.getenv('BEST_BUY_giga_5090'):
 def send_notification(title, message):
     API_TOKEN = os.getenv('API_TOKEN')
     USER_KEY = os.getenv('USER_KEY')
