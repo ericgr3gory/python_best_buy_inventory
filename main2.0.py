3111523302f60222d129c19032586829253a46aa
ericgr3gory <eric@e-gregory.com>
2025-03-04 10:50:05 -0800

    updated load page fuction

--- main2.0.py
+++ main2.0.py
@@ -20,11 +20,6 @@ logging.basicConfig(
 
 load_dotenv()
 LINK = os.getenv('BEST_BUY_LINK')
-#LINK = os.getenv('BEST_BUY_LINK_ALL')
-#LINK = os.getenv('BEST_BUY_LINK_TEST')
-#LINK = os.getenv('BEST_BUY_giga_5090')
-
-# Start a single Playwright instance.
 
 def handle_exception(exc_type, exc_value, exc_tb):
     logging.exception("Uncaught exception", exc_info=(exc_type, exc_value, exc_tb))
@@ -48,27 +43,33 @@ def open_browser(PW):
     logging.info(f'Page created {LINK}')
     return browser, context, page
 
-def load_page(browser, context, page):
-    attempts = 0
-    max_attempts = 10
-    while attempts < max_attempts:
-        attempts += 1
-        logging.info(f'{attempts} attempts to load page')
+def load_page(browser, context, page, action):
+    
+    while True:
+        
         try:
-            page.goto(LINK)
-            logging.info('Page loaded')
+            if action == 'goto':
+                logging.info(f'Trying to load{LINK}')
+                page.goto(LINK)
+                logging.info('Page loaded')
+            if action == 'reload':
+                logging.info(f'Trying to reload{LINK}')
+                page.reload()
+                logging.info('Page reloaded')
             return browser, context, page
+        
         except (TimeoutError, Error) as e:
             
             if isinstance(e, TimeoutError) or "Page crashed" in str(e):
-                logging.info(f'{e} - page failed to reload (attempt {attempts})')
+                logging.info(f'{e} - page failed to reload')
                 browser.close()
                 return start_scraping_page()
             else:
-                raise
-            
-    logging.info('Max attempts reached. Exiting.')
-    sys.exit("Max attempts reached.")
+                logging.error(f'{e}')
+                send_notification('ERROR', e)
+                browser.close()
+                return start_scraping_page()
+                
 
 def send_notification(title, message):
     API_TOKEN = os.getenv('API_TOKEN')
@@ -101,38 +102,13 @@ def international_page(page):
         logging.info('clicked')
     
     except TimeoutError as e:
-        logging.info('didnt find internal link')    
+        logging.info('didnt find internal link')
     
-
-def reloading_page(browser, context, page):
-    attempts = 0
-    max_attempts = 5
-    while attempts < max_attempts:
-        attempts += 1
-        try:
-            logging.info(f'Trying to reload{LINK}')
-            page.reload()
-            logging.info('Page reloaded')
-            return browser, context, page
-        except (TimeoutError, Error) as e:
-            
-            if isinstance(e, TimeoutError) or "Page crashed" in str(e):
-                logging.info(f'{e} - page failed to reload (attempt {attempts})')
-                browser.close()
-                logging.info("browser closed after unsuccesful reload")
-                return start_scraping_page()
-            else:
-                raise
-            
-    logging.info('Max attempts reached. Exiting.')
-    sys.exit("Max attempts reached.")
-    
-
 def start_scraping_page():
     logging.info('starting the scrape')
     vpn.vpn()
     browser, context, page = open_browser(PW)
-    return load_page(browser, context, page)
+    return load_page(browser, context, page, 'goto')
 
 def main():
     
@@ -141,7 +117,7 @@ def main():
     button_disabled, is_soldout = check_button_state(page, sku, 'SOLD_OUT')
     
     while button_disabled and is_soldout:
-        browser, context, page = reloading_page(browser, context, page)
+        browser, context, page = load_page(browser, context, page, 'reload')
         button_disabled, is_soldout = check_button_state(page, sku, 'SOLD_OUT')
     
     button_disabled, is_add_cart = check_button_state(page, sku, 'ADD_TO_CART')
